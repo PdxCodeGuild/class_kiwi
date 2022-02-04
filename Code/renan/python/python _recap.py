acda261e7759555280403158ffa6d74072ca5bd9
renan <renanharrigan@gmail.com>
2022-02-03 22:47:52 -0500

    Lab1 submission

---
+++ Code/renan/python/python _recap.py
@@ -0,0 +1,78 @@
+"Hello World!"
+#print("Hello World!") 
+
+#print(4)
+
+#print(2.3)
+
+#print(True)
+
+#print([1, 2, 3, 4])
+
+#print(type("Hi There, this is a type string"))
+
+#input("Enter your name:")
+
+#print("Hello Brian")
+
+#print("Hello", input("Enter your name: "))
+
+#greeting = "Hello World"
+
+#print(greeting)
+
+#name = input("Enter you name: ")
+
+#print("Hello", name)
+
+#color = input("What is your favorite color? ")
+
+#print(f"I Like {color} too!")
+
+#number = 13
+
+#print(f" I have {number} cats")
+
+#age = input("ener your age: ")
+#age = int(age)
+#print(f'Wow you are old, cant believe you are {age + 50}')
+
+
+# price = float((input("Enter the price of your item: ")))
+# price = round(price * .5, 2)
+# print(f"Your total today is ${price}.  This incluces a 50% discount.")
+
+
+#price = float(input("Enter the price of your item: "))
+#price is between $50 and $75, give a 10% discount
+#if price > 50 and price <= 75:
+   #price = round(price * .9, 2)
+    #print(f"Your total today is ${price}.  This incluces a 10% discount.")
+#price is greater than $75 and $100,  give a 15% discount    
+#elif price > 75 and price <=100:
+    #price = round(price *.85, 2)
+    #print(f"Your total today is ${price}.  This incluces a 15% discount.")
+#price is over $100, give a 20% discount
+#elif price > 100:
+    #price = round(price *.8, 2)
+    #print(f"Your total today is ${price}.  This incluces a 20% discount.")
+#else:
+    #print(f"Your total today is ${price}")
+
+#Ask the user for the price of their item
+
+
+# price = float(input("Enter the price of your item: "))
+
+#Ask the user if they are a member
+# user = input("Are you a member? ")
+
+#if user is a member, apply 20% discount
+# if user == "yes":
+#     price = price * .8
+#     print(f"Your total is ${price:.2f}.  Enjoy your 20% discsount for being a member.")
+    
+#otherwise, show them full price
+# elif user == "no":
+#     print (f"Your total is ${price:.2f}.  You could have saved ${price *.2:.2f} by having a membership")
+    
