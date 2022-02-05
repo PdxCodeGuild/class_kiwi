e74219e4351dfc6b1b8e05b22b99620835016ab2
Lis Fano <lisfano@gmail.com>
2022-02-04 20:04:32 -0800

    lab2 submission

---
+++ Code/lis/lab2.py
@@ -0,0 +1,48 @@
+# Lab 2
+
+# *** Version 1 ***
+
+nums = [5, 0, 8, 3, 4, 1, 6] 
+num_sum = 0
+
+for num in nums:
+    num_sum += num
+    print("running sum: ", num_sum)
+    print("running average: ", num_sum/len(nums))
+
+
+
+# *** Version 2 ***
+
+'''
+nums = []
+
+while True:
+  input_number = input("Enter a number, or 'done': ")
+
+  if input_number == 'done':
+    break 
+
+  input_number = int(input_number)
+  nums.append(input_number)
+
+nums_sum = sum(nums)
+nums_count = len(nums)
+nums_avg = nums_sum/nums_count
+
+print("average:", nums_avg)
+'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
