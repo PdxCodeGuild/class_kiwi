9f4a810676de62d7b5302073c5bdaa69004638cb
Jose <josesanchezramirez04@gmail.com>
2022-02-04 20:14:40 -0600

    just told me i was behind a commit so i am trying to fix

---
+++ Code/jose/python/lists_loops.py
@@ -0,0 +1,62 @@
+# fruits = ["apple","bananas", "citrus"]
+
+# fruits.append("dragon fruit")
+
+# # fruits.append(4)
+
+# # fruits.append(True)
+
+
+# # fruits.append(['red','green','blue'])
+
+# # fruits[-1] = 'cantelope'
+
+
+# # fruits.pop(2)                        removed item with index
+# # fruits.remove('bananas')             removed item with element
+
+
+# print(fruits)
+
+# full_names = [
+#     'bruce banner',
+#     'peter parker',
+#     'tony stark'
+# ]
+# for name in full_names:
+
+#     print(name)
+
+# num = 0
+# while num < 12:
+#     num = num + 1
+#     print(num,"Hello World!")
+
+# greeting = "Hello Class Kiwi"
+
+# for x in greeting:
+#     print(x)
+
+
+# colors = ['red','green','blue','yellow']
+# for x in colors:
+#     print(x)
+
+
+# person = {
+#     'first_name': 'Joe',
+#     'last_name': 'Shmoe',
+#     'age': 37,
+#     'fav_color': 'green'
+# }
+# for x in person:
+#     print(x)
+
+# while True:
+#   print('Hello')
+#   name = input("What is your name \n")
+#   if name =='exit':
+#     break
+#   elif name == 'voldemort':
+#         continue
+# print('The loop has ended')
