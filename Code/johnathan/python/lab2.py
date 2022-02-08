0db7bba0de1376f388e97101b3bf77e8799a236d
JohnnyMurph22 <98765051+JohnnyMurph22@users.noreply.github.com>
2022-02-07 20:23:54 -0800

    Lab2 update

--- Code/johnathan/python/lab2.py
+++ Code/johnathan/python/lab2.py
@@ -1,12 +1,10 @@
-
-# # Lab 2: Average Numbers
+# Lab 2: Average Numbers
 
 # We're going to average a list of numbers. Start with the following list, iterate through it, keeping a 'running sum', then divide that sum by the number of elements in that list. Remember `len` will give you the length of a list.
 
 # The code below shows how to loop through an array, and prints the elements one at a time.
-
-
 # loop over the indices
+
 nums = [5, 0, 8, 3, 4, 1, 6]
 
 #set a new variable = 0; to initialize iterative summation 
@@ -17,28 +15,26 @@ for i in range(len(nums)):
 total_items = len(nums)
 #Average the total numbers added by the list length 
 avg = sum_total/total_items
+avg = round((avg),2)
+#print(avg)
 
-print(round(avg,2))
+## Version 2
 
-# ```
-
-# ## Version 2
-
-# Ask the user to enter the numbers one at a time, putting them into a list. If the user enters 'done', then calculate and display the average. The following code demonstrates how to add an element to the end of a list.
-
-# ```python
-# nums = []
-# nums.append(5)
-# print(nums)
-# ```
-
-# Below is an example input/output:
+# Ask the user to enter the numbers one at a time, putting them into a list. If the user enters 'done', then calculate and display the average. 
+user_num_entry = 0
 
 
-# ```
-# > enter a number, or 'done': 5
-# > enter a number, or 'done': 3
-# > enter a number, or 'done': 4
-# > enter a number, or 'done': done
-# average: 4
-# ```
+while user_num_entry != 'done':
+    if user_num_entry >= 0: 
+        while True:
+            user_num_entry = int(input('Enter a list of numbers one at a time, or type "done" if finished: '))
+            if user_num_entry == 'done':
+                print(f'Your average is: {(user_num_entry/total_items)}.')
+
+
+#     user_num_entry = input("\nEnter a list of numbers one at a time: ")
+#     user_num_entry = int(user_num_entry)
+#     user_num_entry = nums.append(user_num_entry)
+#     if user_num_entry == 'done':
+  
+#          break 
