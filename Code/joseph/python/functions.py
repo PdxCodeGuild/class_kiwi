68c3edf1a421abd27e734b2240171ee603d32009
yusufbromack <josephbrownwomack@gmail.com>
2022-02-07 19:35:23 -0800

    functions notes

--- Code/joseph/python/functions.py
+++ Code/joseph/python/functions.py
@@ -31,3 +31,18 @@ def subtract(num1, num2):
     return result
 
 subtract(7, 5)
+
+# create function that says the temperature in english
+def get_temperature(temp: float) -> str:
+    '''function takes in temp as number returns temp in english'''
+
+    if temp > 90:
+        return "Hot"
+    elif temp >75 and temp <=90:
+        return "Warm"
+    elif 60 < temp <=75:
+        return "Comfortable"
+    elif 45 < temp <=60:
+        return "Chilly"
+    elif temp <= 45:
+        return "Freezing"
