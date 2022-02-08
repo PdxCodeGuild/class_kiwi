b9967fbca04fc797ef039b35212b4e7b784c1e7c
yusufbromack <josephbrownwomack@gmail.com>
2022-02-07 18:29:05 -0800

    functions notes

---
+++ Code/joseph/python/functions.py
@@ -0,0 +1,32 @@
+# built in functions
+# print()
+# input()
+# type()
+# round()
+
+"""
+# anatomy of a function
+def function_name(parameters):
+    '''docstring'''
+    statement1
+    statement2
+    '''
+    '''
+    return [expression]
+"""
+
+def greet(first_name, last_name=""):
+    '''
+    print a welcome message to the console
+    '''
+    print(f'hello {first_name} {last_name}')
+
+
+greet('Barry', 'harry')
+greet(' ')
+greet('Ron', 'Barry')
+
+def subtract(num1, num2):
+    print(num1 - num2)
+
+subtract(7, 5)
