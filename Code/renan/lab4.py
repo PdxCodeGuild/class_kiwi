3b0707384d568077280ef0a72e40cf6829cdf0a3
renan <renanharrigan@gmail.com>
2022-02-09 00:33:40 -0500

    Lab4

---
+++ Code/renan/lab4.py
@@ -0,0 +1,51 @@
+#Python program to give basic blackjack playing advice during a game 
+# Ask the user for three playing cards (A, 2, 3, 4, 5, 6, 7, 8, 9, 10, J, Q, or K). Then, figure out the point value of each card individually. Number cards are worth their number, all face cards are worth 10.
+
+#Define Card Values as well as Choices
+card_value = {
+    
+    "A": 1,
+    "2": 2,
+    "3": 3, 
+    "4": 4,
+    "5": 5,
+    "6": 6,
+    "7": 7,
+    "8": 8,
+    "9": 9, 
+   "10": 10,
+   "J": 10,
+   "Q": 10, 
+   "K": 10
+         
+}
+
+
+user_choice = input("Whats Your First Card 1: ") 
+
+user_choice2 = input("Whats Your Second Card 2: ") 
+
+user_choice3 = input("Whats Your Third Card 3: ") 
+
+
+# print(user_choice)
+
+# print(card_value[user_choice, user_choice2, user_choice3])
+
+sum = (card_value[user_choice] + card_value[user_choice2] + card_value[user_choice3])
+
+print(sum)
+
+if sum <= 16:
+    print("Hit")
+    
+if 17 <= sum < 21:
+    print("Stay")
+
+if sum == 21:
+    print("Blackjack!")
+
+if sum > 21:
+    print("Already Busted")
+    
+
