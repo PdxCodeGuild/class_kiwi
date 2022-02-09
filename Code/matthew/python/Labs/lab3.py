7ef362f3097a7ff9d3d7f57d3e361388645e3d7b
Matthew Holmes <holmes.matthew95@gmail.com>
2022-02-08 18:14:59 -0800

    Minor text clean up

--- Code/matthew/python/Labs/lab3.py
+++ Code/matthew/python/Labs/lab3.py
@@ -9,7 +9,8 @@ error. Even if no code or variables have changed. For example I was running 61. 
 Sometimes my if statements will run like " if 10 >= x <= 12" and sometimes I have to change it to "if x >= 10 and x < 13".
 Again not chaning anything just running the code. If I dont change the code it will either skip over the code even if the variables 
 fit the if statement or the opposite. 
-
+---
+Fixed but I dont know why it was doing that...
 '''
 
 
@@ -17,20 +18,22 @@ fit the if statement or the opposite.
 def num_to_phrase(x: int ) -> str: 
     '''
     Convertring numbers(int) into text(str) EX: 99 to ninety-nine
+    
     '''
+    
     numbers = {1:'One', 2:'Two', 3:'Three', 4:'Four', 5:'Five', 6:'Six', 7:'Seven', 8:'Eight', 9:'Nine', 0:'Zero'}
     tens = {1: 'Ten', 2: 'Twenty', 3: 'Thirty', 5 : 'Fifty', 6: 'Sixty', 7: 'Seventy', 8: 'Eighty', 9: 'Ninety'}
     teens = {1:'Eleven', 2: 'Twelve', 3: 'Thirteen', 4: 'Fourteen', 5: 'Fifteen', 6: 'Sixteen', 7: 'Seventeen', 8: 'Eighteen', 9: 'Nineteen'}
     hundreds = {1: 'One Hundred and ', 2: 'Two Hundred and ', 3: 'Three Hundred and ', 4: 'Four Hundred and ', 5: 'Five Hundred and ', 
     6: 'Six Hundred and ', 7: 'Seven Hundred and ', 8: 'Eight Hundred and ', 9: 'Nine Hundred and '}
+    
     hundredth_digit = x//100
     tens_digit = (x//10) - (hundredth_digit * 10)
     ones_digit = x%10
-    # tens_digit_str = ''
-    # ones_digit_str = ''
     
     tens_digit_str = tens[tens_digit]
     ones_digit_str = numbers[ones_digit]
+    
     if x <= 99: # if x is under 99
         if x == 10: # checking to see if x is 10
             return tens_digit_str
@@ -44,7 +47,7 @@ def num_to_phrase(x: int ) -> str:
         hundredth_str = hundreds[hundredth_digit] # creating a hundreth str
         if tens_digit == 1 and ones_digit == 0: # checking for x10
             return hundredth_str + tens_digit_str
-        elif tens_digit == 1 and ones_digit != 0: # checking for x13-x19 (teens)
+        elif tens_digit == 1 and ones_digit != 0: # checking for x13-x19 (teens) and x11,x12
             return hundredth_str + teens[ones_digit]
         elif ones_digit == 0: # checking for xx0
             return hundredth_str + tens[tens_digit]
