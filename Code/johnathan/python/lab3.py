793059ef3980aeac8f1116d8824ce3ff77f67b03
JohnnyMurph22 <98765051+JohnnyMurph22@users.noreply.github.com>
2022-02-08 19:16:35 -0800

    Lab 3 corrections

--- Code/johnathan/python/lab3.py
+++ Code/johnathan/python/lab3.py
@@ -12,9 +12,13 @@
 # Hint 2: use the digit as an index for a list of strings OR as a key for a dict of digit:phrase pairs.
 
 #Step 1) Create a dictionary and store the key values
+
+
+
 number2phrase_dic = {0: 'zero', 1: 'one', 2: 'two', 3: 'three', 4: 'four', 5: 'five', 6:'six', 7:'seven', 8:'eight', 9:'nine', 10:'ten',
  11: 'eleven', 12: 'twelve', 13: 'thirteen', 14: 'fourteen', 15:'fifteen', 16:'sixteen', 17: 'seventeen', 18: 'eighteen', 19:'nineteen', 20: 'twenty',
- 30:'thiry', 40: 'fourty', 50: 'fifty', 60: 'sixty', 70:'seventy', 80: 'eighty', 90: 'ninety', 100: 'hundred', }
+ 30:'thiry', 40: 'fourty', 50: 'fifty', 60: 'sixty', 70:'seventy', 80: 'eighty', 90: 'ninety', 100: 'one-hundred', 200: 'two-hundred',
+  300:'three-hundred', 400:'four-hundred', 500:'five-hundred', 600:'six-hundred', 700:'seven-hundred', 800:'eight-hundred', 900: 'nine-hundred'}
 
 #Step 2) Take input from user -- convert to integer 
 
@@ -27,17 +31,15 @@ if entry_int in number2phrase_dic:
 #Use modulus to extract ones and tens digits (hint) -- so you don't have to keep typing stuff out!
 #use .get() to access a value with a key
 elif entry_int not in number2phrase_dic:
-    ones_digit = entry_int % 10
-    tens_digit = int(entry_int /10) * 10
-    hundreds_digit = int(entry_int/10) * 100
+    ones_digit = entry_int % 10 # renders ones digit by giving the remainder
+    tens_digit = int(entry_int /10) * 10 # Provides the coefficient *10 to recieve a tens digit 
+    hundreds_digit = (int(entry_int% 1000) // 100) * 100
     print(number2phrase_dic.get(hundreds_digit), number2phrase_dic.get(tens_digit), number2phrase_dic.get(ones_digit))
-
 
 # ## Version 2
 
 # Handle numbers from 100-999.
-new_dict = {200: 'two-hundred', 300:'three-hundred', 400:'four-hundred', 500:'five-hundred', 600:'six-hundred', 700:'seven-hundred', 800:'eight-hundred', 900: 'nine-hundred'}
-number2phrase_dic.update(new_dict)
+
 
 
 # ## Version 3 (optional)
