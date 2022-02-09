8494dca27b51488b99ec0bd070896f2eab2c366b
Shaina Davis <davisshainar89@gmail.com>
2022-02-08 23:17:37 -0600

    Lab 4 v1 and 2 and Lab3 corrections

--- Code/shaina/Python/lab3_v1.py
+++ Code/shaina/Python/lab3_v1.py
@@ -6,12 +6,12 @@ num = int(input('Enter number: '))
 # Dictionaries with int as key, and values as english representation
 ones = {
     1: 'One', 2: 'Two', 3: 'Three', 4: 'Four', 5: 'Five',
-    6: 'Six', 7: 'Seven', 8: 'Eight', 9: 'Nine', 0: ''
+    6: 'Six', 7: 'Seven', 8: 'Eight', 9: 'Nine', 0: 'Zero'
 }
 
 tens = {
     1: 'Ten', 2: 'Twenty', 3: 'Thirty', 4: 'Forty', 5: 'Fifty',
-    6: 'Sixty', 7: 'Seventy', 8: 'Eighty', 9: 'Ninety'
+    6: 'Sixty', 7: 'Seventy', 8: 'Eighty', 9: 'Ninety', 0: ''
 }
 
 # Function breaking number into seperate digits and returning appropriate phrase
@@ -20,7 +20,9 @@ def phrase(num = int):
     ones_digit = num%10
     phrase = tens[tens_digit] + '-' + ones[ones_digit]
     
-    if ones_digit == 0:
+    if ones_digit == 0 and tens_digit == 0:
+        return 'Zero'
+    if ones_digit == 0 and tens_digit > 0:
         return f'{tens[tens_digit]}'
     elif num == 11:
         return 'Eleven'
