52399c09a27f289f052e07d972db8bbe63b8e24b
Matthew Holmes <holmes.matthew95@gmail.com>
2022-02-09 20:07:18 -0800

    Adding Lab 5 V1 and V2

--- Code/matthew/python/Labs/lab3.py
+++ Code/matthew/python/Labs/lab3.py
@@ -22,7 +22,7 @@ def num_to_phrase(x: int ) -> str:
     '''
     
     numbers = {1:'One', 2:'Two', 3:'Three', 4:'Four', 5:'Five', 6:'Six', 7:'Seven', 8:'Eight', 9:'Nine', 0:'Zero'}
-    tens = {1: 'Ten', 2: 'Twenty', 3: 'Thirty', 5 : 'Fifty', 6: 'Sixty', 7: 'Seventy', 8: 'Eighty', 9: 'Ninety'}
+    tens = {1: 'Ten', 2: 'Twenty', 3: 'Thirty', 5 : 'Fifty', 6: 'Si    xty', 7: 'Seventy', 8: 'Eighty', 9: 'Ninety'}
     teens = {1:'Eleven', 2: 'Twelve', 3: 'Thirteen', 4: 'Fourteen', 5: 'Fifteen', 6: 'Sixteen', 7: 'Seventeen', 8: 'Eighteen', 9: 'Nineteen'}
     hundreds = {1: 'One Hundred and ', 2: 'Two Hundred and ', 3: 'Three Hundred and ', 4: 'Four Hundred and ', 5: 'Five Hundred and ', 
     6: 'Six Hundred and ', 7: 'Seven Hundred and ', 8: 'Eight Hundred and ', 9: 'Nine Hundred and '}
