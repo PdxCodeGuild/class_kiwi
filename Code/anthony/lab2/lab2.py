33523c191f3cddcf73baac0795ac2d49177da8c3
anthonybillie <anthonymbillie@gmail.com>
2022-02-11 20:49:53 -0500

    lab 5

--- Code/anthony/lab2/lab2.py
+++ Code/anthony/lab2/lab2.py
@@ -1,13 +1,15 @@
 # Lab 2: Average Numbers
 
 # # Number List
+from pip import main
+
+
 nums = [5, 0, 8, 3, 4, 1, 6]
 
 sum = 0
 # # run through list and keeping a running sum
 for num in nums:
-    sum = num + sum
-    print(sum)
+    sum += num
 
 # # divide sum by number of elements in that list
 sum = sum/len(nums)
