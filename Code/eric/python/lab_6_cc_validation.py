04485a4654d0a8ef215f9bd62531dd9e05bbbea6
Matir11 <51160306+Matir11@users.noreply.github.com>
2022-02-12 00:38:18 -0500

    Credit Card Validation

---
+++ Code/eric/python/lab_6_cc_validation.py
@@ -0,0 +1,44 @@
+##Lab 6 Credit Card validation
+
+#Take a number and verify it is a valid credit card number
+
+#convert input string into a list
+
+number = input('Enter your credit card number: ')
+
+#create a for loop to build the list
+number_broken = []
+
+for i in number:
+    number_broken.append(int(i))
+
+#slice off the last digit
+last_digit = number_broken.pop(-1)
+
+#reverse the digits
+number_broken.reverse()
+
+#double every other element
+for i in range(0, len(number_broken), 2):
+    number_broken[i] *= 2
+
+#subtract 9 from numbers over 9
+for i in range(0, len(number_broken)):
+    if number_broken[i] > 9:
+        number_broken[i] -=9
+    
+#sum all values
+summed_up = sum(number_broken)
+
+#take the second digit of sum
+second_digit = summed_up % 10
+
+#compare the second digit to the pooped digit
+
+if second_digit == last_digit:
+    print(f'{number} is a valid credit card number')
+    
+else:
+    print(f'{number} is not valid')
+    
+    
