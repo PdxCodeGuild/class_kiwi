8c9dc64df47df12438e5fd259602ab001097a5cb
renan <renanharrigan@gmail.com>
2022-02-16 22:49:16 -0500

    Submitting my lab6

--- Code/renan/lab6.py
+++ Code/renan/lab6.py
@@ -1,21 +1,74 @@
 #Credit Card Validator
+#4556737586899855
+# card_number = input("What is the card number?: ")
 
-card_number = input("What is the card number?: ")
+# c_card =[]
+# for char in c_card:
+#         c_card.append(int(char))
+
+#Another Option (python is reading this from right to left..for every number in this list..convert to int)
+#nums = [int(char) for char in card_number]
+card_number = '4556737586899855'
+
+
+
+
 #Convert input strings to list
-nums = list(card_number)
+def credit_card_validator():
+    nums = list(card_number)
 
-for i in range(len(nums)):
-    nums[i] = int(nums[i])
-    print(int(nums[i]))
+    for i in range(len(nums)):
+        nums[i] = int(nums[i])
+        print(int(nums[i]))
 
-#Slice off the last digit (now called check digit)
-check_digit = nums [0:15]
+    print(nums)
 
-print(check_digit) 
+    #Slice off the last digit (now called check digit)
+    check_digit = nums.pop()
 
-#Reverse List
-check_digit.reverse()
+    #Reverse List
+    nums = nums[::-1] 
+    print(nums)
 
-print(check_digit)
+    #Another method to reverse the list
+    # check_digit.reverse()
+    # nums = list(reversed(nums)
+
+    #Double every other element in the reversed list
+    for x in range(0, len(nums), 2):
+        nums[x] += nums[x]
+
+    print(nums)
+
+    #Option 1
+    # for index, value in enumerate(nums):
+    #     if index % 2 == 0:
+    #         nums[index] = value *2
+
+    # Subtract nine from numbers over nine.
+    #Option 1
+    for index, value in enumerate(nums):
+        if value > 9:
+            nums[index] = value - 9
+
+    print(nums)
+
+    # Sum all values.
+    total = sum(nums)
+
+    print(total)
 
 
+    # Take the second digit of that sum.
+
+    last_digit = total % 10
+
+    print(last_digit)
+
+    # If that matches the check digit, the whole card number is valid.
+    if last_digit == check_digit:
+        return True
+    else:
+        return False
+    
+print(credit_card_validator())
