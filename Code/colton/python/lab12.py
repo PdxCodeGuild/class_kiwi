fd7f62ee2f0691e4fef5771eaf15f8b958b4e11f
Colton Tatum <96851531+Tatumc4561@users.noreply.github.com>
2022-02-25 18:49:17 -0800

    added lab12.py version 1 and 2

---
+++ Code/colton/python/lab12.py
@@ -0,0 +1,84 @@
+class ATM:
+    def __init__(self, balance=0, interest=0.1):
+        self.balance = balance
+        self.interest = interest
+
+    # Returns acount balance
+    def check_balance(self):
+        amount = self.balance
+        return amount
+    
+    # Deposits the given amount in the account
+    def deposit(self, amount):
+        self.balance += amount
+        return 
+    
+    # Returns true if the withdrawn amount won't put the account in the negative
+    def check_withdrawal(self, amount):
+        if self.balance - amount >= 0:
+            return True
+
+    # Withdraws the amount from the account and returns it
+    def withdraw(self, amount):
+        self.balance -= amount
+        return
+    
+    # Returns the amount of interest calculated on the account
+    def calc_interest(self):
+        return round((self.balance * self.interest), 2)
+    
+    
+    def print_transactions(transactionlist):
+            history = """
+Total Transactions
+------------------
+"""
+            for i in transactionlist:
+                history += f"{i}\n"
+            
+
+            return history
+
+
+
+
+transactionlist = []
+atm = ATM() # create an instance of our class
+print('Welcome to the ATM')
+while True:
+    command = input('Enter a command: ')
+    if command == 'balance':
+        balance = atm.check_balance() # call the check_balance() method
+        print(f'Your balance is ${balance}')
+    elif command == 'deposit':
+        amount = float(input('How much would you like to deposit? '))
+        atm.deposit(amount) # call the deposit(amount) method
+        transactionlist.append(f'Deposited ${amount}')
+        print(f'Deposited ${amount}')
+    elif command == 'withdraw':
+        amount = float(input('How much would you like '))
+        if atm.check_withdrawal(amount): # call the check_withdrawal(amount) method
+            atm.withdraw(amount) # call the withdraw(amount) method
+            transactionlist.append(f'Withdrew ${amount}')
+            print(f'Withdrew ${amount}')
+        else:
+            print('Insufficient funds')
+    elif command == 'interest':
+        amount = atm.calc_interest() # call the calc_interest() method
+        atm.deposit(amount)
+        print(f'Accumulated ${amount} in interest')
+    elif command == 'help':
+        print('Available commands:')
+        print('balance  - get the current balance')
+        print('deposit  - deposit money')
+        print('withdraw - withdraw money')
+        print('interest - accumulate interest')
+        print('transactions - print out all transactions')
+        print('exit     - exit the program')
+    elif command == 'transactions':
+        history = ATM.print_transactions(transactionlist) # call the print_transactions method
+        print(history)
+    elif command == 'exit':
+        break
+    else:
+        print('Command not recognized')
