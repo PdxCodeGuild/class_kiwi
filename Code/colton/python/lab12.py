bf389c18215aa280741da2213062340d4f53c09f
Colton Tatum <96851531+Tatumc4561@users.noreply.github.com>
2022-02-25 19:02:15 -0800

    Added docstrings to Class and Class/defs

--- Code/colton/python/lab12.py
+++ Code/colton/python/lab12.py
@@ -1,43 +1,50 @@
 class ATM:
+    """ATM"""
     def __init__(self, balance=0, interest=0.1):
         self.balance = balance
         self.interest = interest
 
     # Returns acount balance
     def check_balance(self):
+        """Check balance"""
         amount = self.balance
         return amount
     
     # Deposits the given amount in the account
     def deposit(self, amount):
+        """Deposit into balance"""
         self.balance += amount
         return 
     
     # Returns true if the withdrawn amount won't put the account in the negative
     def check_withdrawal(self, amount):
+        """Check withdrawal to make sure it doesn't send account to a negative num"""
         if self.balance - amount >= 0:
             return True
 
     # Withdraws the amount from the account and returns it
     def withdraw(self, amount):
+        """Withdraw from balance"""
         self.balance -= amount
         return
     
     # Returns the amount of interest calculated on the account
     def calc_interest(self):
+        """Calculate accrued interest on balance"""
         return round((self.balance * self.interest), 2)
     
-    
+    # Returns transaction history
     def print_transactions(transactionlist):
-            history = """
+        """Return history of transactions"""
+        history = """
 Total Transactions
 ------------------
 """
-            for i in transactionlist:
-                history += f"{i}\n"
-            
+        for i in transactionlist:
+            history += f"{i}\n"
+        
 
-            return history
+        return history
 
 
 
