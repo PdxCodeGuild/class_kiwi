89e635aa00f1ef1d860106a4eddf70fc3aca3386
Cy83rV37 <dnote22@hotmail.com>
2022-02-28 20:15:22 -0800

    Lab 14 version 2

---
+++ Code/daryll/python/lab14v2.py
@@ -0,0 +1,21 @@
+import requests
+
+joke_search = input("Please enter a joke search term: ")
+response = requests.get(f"https://icanhazdadjoke.com/search?term={joke_search}", headers = { 
+    "Accept": "application/json"
+})
+
+joke = response.json()
+joke_number = 0
+user_response = "yes"
+while user_response == "yes":
+    try:
+        if user_response == "no":
+            break
+        else:
+            print(joke["results"][joke_number]["joke"] + "\n")
+            joke_number += 1
+            user_response = input(f"Would you like to hear the next {joke_search} joke? ")
+    except IndexError as error: # If there are no more jokes then an IndexError will occur
+        print(f"There are no more {joke_search} jokes.")
+        break
