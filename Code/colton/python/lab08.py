d3a185614a8b6dda2322eefe28598fea80b9abb2
Colton Tatum <96851531+Tatumc4561@users.noreply.github.com>
2022-03-05 10:39:40 -0800

    back-up folder of all files

--- Code/colton/python/lab08.py
+++ Code/colton/python/lab08.py
@@ -10,6 +10,9 @@
 
 
 
+from asyncio import DatagramProtocol
+
+
 def peaks(data):
     """Gets peaks in a list"""
     list = []
@@ -81,6 +84,30 @@ def draw_peaks_valleys(data):
         
     return
 
+def draw_peaks_valleys_with_water(data):
+    """Draws peaks and valleys of a list"""
+    rows = max(data)
+    cols = len(data)
+    pvs = peaks_and_valleys(data)
+    for i in range(rows, 0, -1):
+        for num in range(cols):
+            if i > data[num]:
+                print("  ", end=" ")
+           
+            else:
+                for i in range(len(pvs) - 1):
+                    current = pvs[i]
+                    next = pvs[i+1]
+
+                
+                    if current > num < next:
+                        print(" O", end=" ")
+                print(" X", end=" ")
+        print()
+    print(data)
+        
+    return
+
 
 def main():
 
@@ -89,8 +116,8 @@ def main():
     print(peaks(data), "Indices of Peaks")
     print(valleys(data), "Indices of Valleys")
     print(peaks_and_valleys(data),"Values of Peaks, and Valleys in order")
-    print(draw_peaks_valleys(data))
-
+    draw_peaks_valleys(data)
+    draw_peaks_valleys_with_water(data)
 
 if __name__ == "__main__":
     main()
