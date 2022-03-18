af76f4131c5cce3cbfbcca791e72eb49982afb1c
JohnnyMurph22 <98765051+JohnnyMurph22@users.noreply.github.com>
2022-03-17 20:44:17 -0700

    Lab 5 Forms: Complete

---
+++ Code/johnathan/HTML/Flask/app.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+
+#localhost:5000/
+@app.route('/')
+def index ():
+    name="Bill"
+    return render_template('index.html', name=name,)
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route("/contact")
+def contact():
+    return render_template('contact.html')
+
+@app.route('/check-grade/<int:grade>')
+def check_grade(grade):    
+    return render_template('check-grade.html', grade=grade)
+
+@app.route('/llama', methods=['POST'])
+def display_name():
+    return render_template('contact.html')
+    
+
+# if __name__ == "__main__":
+#     app.run(debug=True)
+
