bfb2a213afa5704e1b078f3fa05f05cbd64203ef
yusufbromack <josephbrownwomack@gmail.com>
2022-03-18 21:36:04 -0700

    FLASK Lab 1, Redo, Complex Random Password Generator, final version

---
+++ Code/joseph/flask/lab1/app.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template, request
+import string
+import random
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+@app.route("/password.html", methods=['POST'])
+def password():
+    result = request.form
+    upper = int(result['upper'])
+    lower = int(result['lower'])
+    numbers = int(result['numbers'])
+    special = int(result['special'])
+    pupper = ''.join(random.choices(string.ascii_uppercase, k=upper))
+    plower = ''.join(random.choices(string.ascii_lowercase, k=lower))
+    pnumbers = ''.join(random.choices(string.digits, k=numbers))
+    pspecial = ''.join(random.choices(string.punctuation, k=special))
+    wordpass = (pupper + plower + pnumbers + pspecial)
+    password = ''.join(random.sample(wordpass,len(wordpass)))
+    return render_template("/password.html", password=password)
