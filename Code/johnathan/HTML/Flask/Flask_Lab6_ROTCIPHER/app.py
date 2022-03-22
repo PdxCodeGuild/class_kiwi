bcd57eb52dc1378504b6013123a48c810026caf2
JohnnyMurph22 <98765051+JohnnyMurph22@users.noreply.github.com>
2022-03-21 21:05:56 -0700

    Lab6 Flask ROT13

--- Code/johnathan/HTML/Flask/Flask_Lab6_ROTCIPHER/app.py
+++ Code/johnathan/HTML/Flask/Flask_Lab6_ROTCIPHER/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import string
 
 app = Flask(__name__)
@@ -11,23 +11,26 @@ def index ():
     return render_template('Lab6_Flask_index.html')
 
 @app.route('/ciper_text', methods=['POST'])
-def rot13 (message):
+
+def rot13 ():
+    result = request.form
+    user_data = result['blog']
     alphabet = string.ascii_lowercase
+    # message = {'Lab6_Flask_index.html'}
     user_string = ''
-    for i in message:
+    for i in user_data:
         if i in alphabet:
             if alphabet.index(i) < 13:
                 user_string += alphabet[alphabet.index(i) + 13]
             else: user_string += alphabet[alphabet.index(i) - 13]
+    return render_template('encrypt.html', user_string=user_string)
 
-    return user_string 
+# print(rot13(message))
+#     print(result)
     
-    
 
 
 
-if __name__ == "__main__":
-    app.run()
 
 
 # @app.route('/about')
