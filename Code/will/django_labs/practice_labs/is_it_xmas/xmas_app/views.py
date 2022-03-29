673f7ba03f14713d332163dcdc681ebb2340c9b3
Nithfaris <william.m.carter.45@gmail.com>
2022-03-28 23:02:55 -0500

    Django notes and Django First Program lab

---
+++ Code/will/django_labs/practice_labs/is_it_xmas/xmas_app/views.py
@@ -0,0 +1,9 @@
+from http.client import HTTPResponse
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+
+def index(request):
+    return HttpResponse('hello world')
