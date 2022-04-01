b5f616ae5821f1b608dfa6bef22e8d1e04e530f6
Nithfaris <william.m.carter.45@gmail.com>
2022-03-31 23:07:29 -0500

    Small grocery app progress

--- Code/will/django_labs/grocery_list_proj/grocery_list_proj/urls.py
+++ Code/will/django_labs/grocery_list_proj/grocery_list_proj/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('index/'), include('listapp.urls')
+    path('index/', include('listapp.urls'))
 ]
--- Code/will/django_labs/grocery_list_proj/listapp/urls.py
+++ Code/will/django_labs/grocery_list_proj/listapp/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
 
-app_name = 'listapp'
+
 urlpatterns = [
     path('', views.index, name='index')
 ]
