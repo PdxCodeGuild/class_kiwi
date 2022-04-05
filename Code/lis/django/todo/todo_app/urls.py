f938a6be48ce39e747c0ca54d4457dbd53758431
Lis Fano <lisfano@gmail.com>
2022-04-04 21:06:51 -0700

    todo app

---
+++ Code/lis/django/todo/todo_app/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from .import views
+
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('update/<str:pk>', views.updateTask, name='update'),
+    path('delete/<str:pk>', views.deleteTask, name='delete')
+]
