6cbae2e074835f0e3c6391e55ccbae071791afaf
renan <renanharrigan@gmail.com>
2022-04-18 01:14:07 -0400

    My Todo App

---
+++ Code/renan/django/todo/todo_app/forms.py
@@ -0,0 +1,8 @@
+from django import forms
+from django.forms import ModelForm
+from .models import *
+
+class TaskForm(forms.ModelForm):
+    class Meta:
+        model = Task
+        fields = '__all__'
