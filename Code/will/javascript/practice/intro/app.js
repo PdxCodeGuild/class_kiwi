716b7190658c5e991c51ee756558ebdda78a95c2
Nithfaris <william.m.carter.45@gmail.com>
2022-04-18 22:42:48 -0500

    javascript intro notes

---
+++ Code/will/javascript/practice/intro/app.js
@@ -0,0 +1,99 @@
+// //alert('Hello World')
+// console.log('Hello Class Kiwi')
+// console.warn('Something is up...')
+// console.info('Page Loaded')
+
+// //let userInput = prompt('How is your day?')
+
+// const favColor = 'Orange' //Unchanging variable, constant
+// console.log(favColor, 'My favorite color')
+
+// let lastName = 'Smith' //changeable variable
+
+// let color = 'bluegreen'
+// console.log(typeof color) // returns string
+
+// let numOne = 1
+// console.log(typeof numOne) // returns int
+
+// Data Types
+// 'Hello World' //string
+// "Hello World" //string
+
+// 4       //number
+// 4.7     //number (not a float)
+
+// true    //Boolean
+// false   //Boolean
+
+// [1,2,3]  //array (works like a list)
+
+// {name: 'Tom'}  //Object (works like a dictionary)
+
+// undefined  //undefined (value does not exist)
+// null        //intentional absence of value
+
+// console.log(x)
+// let x = 24 + ' Tomorrow'
+
+// let number = 5
+// console.log(number == '5') // 5 is equal to int 5 and string 5
+
+// let number = 5
+// console.log(number === '5') //  number IS a string, no coercion
+
+// let a = 1 // number (Js) int(Py)
+// let b = 1.2 //number(Js) float(Py)
+// let c = 123e7 //large number
+// let d = 123e-7 //scientific notation
+
+// let number = 5
+// if (number < 10){
+//     console.log('Hooray!')
+// }else if(number>10){
+//     console.log('This is a large number.')
+// }else{
+//     console.log('This number is 10.')
+// }
+
+// let person = {
+//         firstName: 'John',
+//         lastName: 'Snow',
+//         age: 30,
+
+//     pets: {
+//         dog: 'Spot',
+//         cat: 'Claudius'
+//     }
+// }
+// console.log(person)
+// console.log(person['firstName'])
+// console.log(person.lastName)
+
+// //update.age
+// person.age = 40
+
+//increment age
+// person.age ++ // adds 1
+// person.age += 2
+
+// reference nested object
+// console.log(person.pets['dog'])
+
+// function sayHello(){
+//     let color = 'blue'
+//     return color
+// }
+
+// console.log(sayHello())
+
+// function addNums(num1, num2){
+//     return num1 + num2
+// }
+
+// console.log(addNums(3,5))
+
+// const arrowFunct = (num1, num2) => {
+//     return num1 + num2
+// }
+// console.log(arrowFunct(2,8))
