047d0c0e3da787f79ab231dc401ed417dd75d2a6
anthonybillie <anthonymbillie@gmail.com>
2022-04-23 08:11:15 -0400

    django grocery_proj

---
+++ Code/anthony/d_labs/_grocery_proj/grocery_app/models.py
@@ -0,0 +1,23 @@
+
+from django.db import models
+from django.forms import CharField
+
+
+# Create your models here.
+
+
+class Department(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f'{self.name}'
+
+
+class GroceryItem(models.Model):
+    item = models.CharField(max_length=40)
+    completed = models.BooleanField(default=False)
+    department = models.ForeignKey(
+        Department, on_delete=models.CASCADE, related_name='items', null=True, blank=True)
+
+    def __str__(self):
+        return f'{self.item} -- {self.completed}'
