c1c8bec8d8c980fb0bf187f8a1b569c59d07d448
yusufbromack <josephbrownwomack@gmail.com>
2022-04-25 19:30:46 -0700

    todo app in class review

--- Code/joseph/javascript/todoapp/app.js
+++ Code/joseph/javascript/todoapp/app.js
@@ -5,16 +5,42 @@ const todoList = document.querySelector('#todo-list')
 
 //listeners
 todoBtn.addEventListener('click', addTodo)
+//todoInput.addEventListener('keypress', function(e){    console.log(e)})
+todoInput.addEventListener('keypress', function(e){    
+    if(e.key === 'Enter'){
+        addTodo()
+    }
+})
 
 //functions
 function addTodo(){
     const todoText = todoInput.value
-    console.log(todoText)
 
     const li = document.createElement('li')
-    console.log(li)
     
     li.textContent = todoText
 
     todoList.appendChild(li)
+
+    //create delete button, text = x
+    const delBtn = document.createElement('button')
+    delBtn.textContent = 'X'
+    li.append(delBtn)
+
+    //delete todo
+    delBtn.addEventListener('click', function(){
+        todoList.removeChild(delBtn.parentElement) //remove button from parent
+
+    })
+
+    //complete button
+    const completeBtn = document.createElement('button')
+    completeBtn.textContent = &#10004
+    li.append(completeBtn)
+
+    //complete todo
+    completeBtn.addEventListener('click', function(){
+        todoList.
+    })
+
 }
