d39a93139d0273ef2e542902f0ff486af3021ed8
sean <seanschmidt89@yahoo.com>
2022-04-27 18:58:43 -0700

    adding django lab2

---
+++ Code/sean/django_lab2/todo_app/forms.py
@@ -0,0 +1,11 @@
+from dataclasses import fields
+from django import forms
+from django.forms import ModelForm
+from .models import *
+
+
+class TodoForm(forms.ModelForm):
+    class Meta():
+        model = Todo
+        fields = '__all__'
+        
