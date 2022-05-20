b81834d069a6cf241609a58921dc19b1d80e9122
Shaina Davis <davisshainar89@gmail.com>
2022-05-19 19:47:53 -0500

    Shaina Labs

---
+++ Code/shaina/2Flask_HTML_CSS/Flask_Lecture/app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+# localhost:5000
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
---
+++ Code/shaina/Django/Grocery/grocery_app/views.py
@@ -0,0 +1,37 @@
+from django.shortcuts import render
+from .models import *
+
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+
+# Create your views here.
+def index(request):
+    grocery_list = GroceryItem.objects.all().order_by('department')
+    departments = Department.objects.all().order_by('name')
+
+    return render(request, 'grocery_app/index.html', {
+        'grocery_list': grocery_list,
+        'departments': departments
+    })
+
+def add_item(request):
+    item_text = request.POST['item']
+    department_id = request.POST['department']
+    new_item = GroceryItem()
+    new_item.item = item_text
+    if department_id:
+        department = Department.objects.get(id=department_id)
+        new_item.department = department
+    new_item.save()
+    return HttpResponseRedirect(reverse('grocery_list:index'))
+    
+def buy_item(request, item_id):
+    grocery_item = GroceryItem.objects.get(id=item_id)
+    grocery_item.completed =  not grocery_item.completed
+    grocery_item.save()
+    return HttpResponseRedirect(reverse('grocery_list:index'))
+
+def delete_item(request, item_id):
+    grocery_item = GroceryItem.objects.get(id=item_id)
+    grocery_item.delete()
+    return HttpResponseRedirect(reverse('grocery_list:index'))
